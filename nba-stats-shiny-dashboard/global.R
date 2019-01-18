433e27d509359b6a54e5f9b6ff9c41a41499dc86
amarsee <amarsee@live.com>
2019-01-17 18:13:09 -0600

    Renamed shiny folder

---
+++ nba-stats-shiny-dashboard/global.R
@@ -0,0 +1,32 @@
+library(shinydashboard)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(usmap)
+library(plotly)
+library(DT)
+
+# load datset of prescription counts by state
+nba_85_to_now_grouped <- readRDS('data/85_to_now_grouped.rds')
+nba_14_to_present_merged <- readRDS('data/nba_14_to_present_merged.rds')
+nba_14_to_present_merged <- nba_14_to_present_merged %>% 
+  mutate(
+    time_zone = case_when(
+      start_et == "7:00p" | start_et == "7:30p" ~ "Eastern",
+      start_et == "8:00p" | start_et == "8:30p" ~ "Central",
+      start_et == "9:00p" | start_et == "9:30p" ~ "Mountain",
+      start_et == "10:00p" | start_et == "10:30p" ~ "Pacific",
+      TRUE                      ~  "Other"
+    )
+  )
+
+teams_85 <- unique(nba_85_to_now_grouped$team)
+team_14 <- sort(unique(nba_14_to_present_merged$team))
+season_options_tab1 <- unique(nba_14_to_present_merged$year)
+season_options_tab2 <- unique(nba_14_to_present_merged$year)
+
+years <- 1984:2019
+
+
+
+
---
+++ nba-stats-shiny-dashboard/server.R
@@ -0,0 +1,295 @@
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  
+  output$nba_85_days_comp <- renderPlot({
+    
+    team_selection <- input$team
+    year_sel <- as.double( input$season)
+    days <- input$days_between
+    
+    nba_filtered <- nba_85_to_now_grouped %>% 
+      filter(year == year_sel, team == team_selection, daysbetweengames == days)
+    
+    n_df <- data.frame(stat=c("Mean Points For", "Mean Points Against"),
+                     val=c(nba_filtered$mean_points_for, nba_filtered$mean_points_against))
+    
+    ggplot(n_df, aes(x = stat, y = val)) + 
+      geom_bar(stat = "identity", fill = "steelblue") +
+      theme_minimal()
+           
+  })
+  
+  output$nba_14_merged <- renderPlotly({
+    #browser()
+    season_select <- input$season_tab1 # 2014 to 2019
+    tz_1 <- input$time_zone_1 # Can be "Any", "Eastern", "Central", "Mountain", "Pacific", "Other"
+    days_rest_1 <- input$days_between_1 # Can be "Any", "1", "2", "3"
+    home_away_1 <- input$h_a_1 # Can be "Home", "Away", "Either"
+    
+    nba_season <- nba_14_to_present_merged %>% 
+      filter(year == season_select)
+    
+   if (tz_1 == "Any"){
+     nba_season = nba_season
+   } else {
+     nba_season = nba_season %>% 
+       filter(time_zone == tz_1)
+   }
+    
+    if(days_rest_1 == "Any"){
+      nba_season = nba_season
+    } else {
+      days_between = as.numeric(days_rest_1)
+      nba_season = nba_season %>% 
+        filter(daysbetweengames == days_between)
+    }
+    
+    if (home_away_1 == "Either"){
+      nba_season = nba_season
+    } else if (home_away_1 == "Home"){
+      nba_season = nba_season %>% 
+        filter(away_indicator == 0)
+    } else {
+      nba_season = nba_season %>% 
+        filter(away_indicator == 1)
+    }
+    
+    table_out <- nba_season %>% 
+      select(team, date, start_et, away_indicator, opponent, win_loss, tm, opp)
+    
+    output$table_tab1 <- DT::renderDataTable({
+      table_out
+    })
+    
+    nba_grouped <- nba_season %>% 
+      select(team, diff) %>% 
+      group_by(team) %>% 
+      summarise(
+        mean_differential = round(mean(diff, na.rm = TRUE), 2)
+        )
+    nba_grouped$diff_type <- ifelse(nba_grouped$mean_differential < 0, "Negative", "Positive")  
+    nba_grouped <- nba_grouped[order(nba_grouped$mean_differential), ]  # sort
+    nba_grouped$team <- factor(nba_grouped$team, levels = nba_grouped$team)
+    
+    p1 <- ggplot(nba_grouped, aes(x=team, y=mean_differential, label=mean_differential)) + 
+      geom_point(stat='identity', aes(col=diff_type), size=6)  +
+      scale_color_manual(name="Win/Loss", 
+                         labels = c("Net Win", "Net Loss"), 
+                         values = c("Positive"="#00ba38", "Negative"="#f8766d")) + 
+      geom_text(color="white", size=2) +
+      labs(title="Average Margin of Victory By Team") + 
+      ylim(-15, 15) +
+      #ylim(as.integer(min(nba_grouped$mean_differential)) - 1, as.integer(max(nba_grouped$mean_differential)) + 1) +
+      theme_bw() +
+      coord_flip()
+    
+    ggplotly(p1)
+    
+   
+  })
+  
+  
+  
+  output$team_1_points_for <- renderInfoBox({
+    infoBox(
+      "12", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  output$img_team_1 <- renderUI({
+    team_1_char <- input$team_1
+    year_char <- as.character(input$season_tab2)
+    year_num <- input$season_tab2
+    if (team_1_char == "CHA" & year_num > 2014) {
+      team_1_char = "CHO"
+    }
+    
+    img_url_1 <- paste("https://d2p3bygnnzw9w3.cloudfront.net/req/201901091/tlogo/bbr/", team_1_char, "-", year_char, ".png", sep = "")
+    
+    tags$img(src = img_url_1, width = 150, height = 150)
+  })
+  
+  output$img_team_2 <- renderUI({
+    team_2_char <- input$team_2
+    year_char <- as.character(input$season_tab2)
+    year_num <- input$season_tab2
+    if (team_2_char == "CHA" & year_num > 2014) {
+      team_2_char = "CHO"
+    }
+    
+    img_url_2 <- paste("https://d2p3bygnnzw9w3.cloudfront.net/req/201901091/tlogo/bbr/", team_2_char, "-", year_char, ".png", sep = "")
+    
+    tags$img(src = img_url_2, width = 150, height = 150)
+  })
+  
+  team_1_df <- reactive({
+    team_1_char <- input$team_1
+    season_select <- input$season_tab2 # 2014 to 2019
+    tz_team_1 <- input$time_zone_team_1 # Can be "Any", "Eastern", "Central", "Mountain", "Pacific", "Other"
+    days_rest_team_1 <- input$days_between_team_1 # Can be "Any", "1", "2", "3"
+    home_away_team_1 <- input$h_a_team_1 # Can be "Home", "Away", "Either"
+    
+    nba_season <- nba_14_to_present_merged %>% 
+      filter(year == season_select, team == team_1_char)
+    
+    if (tz_team_1 == "Any"){
+      nba_season = nba_season
+    } else {
+      nba_season = nba_season %>% 
+        filter(time_zone == tz_team_1)
+    }
+    
+    if(days_rest_team_1 == "Any"){
+      nba_season = nba_season
+    } else {
+      days_between = as.numeric(days_rest_team_1)
+      nba_season = nba_season %>% 
+        filter(daysbetweengames == days_between)
+    }
+    
+    if (home_away_team_1 == "Either"){
+      nba_season = nba_season
+    } else if (home_away_team_1 == "Home"){
+      nba_season = nba_season %>% 
+        filter(away_indicator == 0)
+    } else {
+      nba_season = nba_season %>% 
+        filter(away_indicator == 1)
+    }
+    
+    nba_grouped <- nba_season %>% 
+      select(team, tm, opp, diff) %>% 
+      group_by(team) %>% 
+      summarise(
+        mean_points_for = round(mean(tm, na.rm = TRUE), 2),
+        mean_points_against = round(mean(opp, na.rm = TRUE), 2),
+        mean_differential = round(mean(diff, na.rm = TRUE), 2)
+      )
+    
+    nba_grouped
+  })
+  
+  team_2_df <- reactive({
+    team_1_char <- input$team_2
+    season_select <- input$season_tab2 # 2014 to 2019
+    tz_team_1 <- input$time_zone_team_2 # Can be "Any", "Eastern", "Central", "Mountain", "Pacific", "Other"
+    days_rest_team_1 <- input$days_between_team_2 # Can be "Any", "1", "2", "3"
+    home_away_team_1 <- input$h_a_team_2 # Can be "Home", "Away", "Either"
+    
+    nba_season <- nba_14_to_present_merged %>% 
+      filter(year == season_select, team == team_1_char)
+    
+    if (tz_team_1 == "Any"){
+      nba_season = nba_season
+    } else {
+      nba_season = nba_season %>% 
+        filter(time_zone == tz_team_1)
+    }
+    
+    if(days_rest_team_1 == "Any"){
+      nba_season = nba_season
+    } else {
+      days_between = as.numeric(days_rest_team_1)
+      nba_season = nba_season %>% 
+        filter(daysbetweengames == days_between)
+    }
+    
+    if (home_away_team_1 == "Either"){
+      nba_season = nba_season
+    } else if (home_away_team_1 == "Home"){
+      nba_season = nba_season %>% 
+        filter(away_indicator == 0)
+    } else {
+      nba_season = nba_season %>% 
+        filter(away_indicator == 1)
+    }
+    
+    nba_grouped <- nba_season %>% 
+      select(team, tm, opp, diff) %>% 
+      group_by(team) %>% 
+      summarise(
+        mean_points_for = round(mean(tm, na.rm = TRUE), 2),
+        mean_points_against = round(mean(opp, na.rm = TRUE), 2),
+        mean_differential = round(mean(diff, na.rm = TRUE), 2)
+      )
+    
+    nba_grouped
+  })
+  
+  output$team_1_points_for <- renderInfoBox({
+    #browser()
+    points_for <- team_1_df()[1,2]
+    infoBox(
+      title = "Mean Points For",
+      value = as.character(points_for) ,
+      icon = icon("bolt"),
+      color = "yellow",
+      width = 12
+    )
+  })
+  
+  output$team_1_points_against <- renderInfoBox({
+    #browser()
+    points_against <- team_1_df()[1,3]
+    infoBox(
+      title = "Mean Points Against",
+      value = as.character(points_against) ,
+      icon = icon("fighter-jet"),
+      color = "red",
+      width = 12
+    )
+  })
+  
+  output$team_1_diff <- renderInfoBox({
+    #browser()
+    point_diff <- team_1_df()[1,4]
+    infoBox(
+      title = "Margin of Victory",
+      value = as.character(point_diff) ,
+      icon = icon("arrows-alt-v"),
+      color = "blue",
+      width = 12
+    )
+  })
+  
+  output$team_2_points_for <- renderInfoBox({
+    #browser()
+    points_for <- team_2_df()[1,2]
+    infoBox(
+      title = "Mean Points For",
+      value = as.character(points_for) ,
+      icon = icon("bolt"),
+      color = "yellow",
+      width = 12
+    )
+  })
+  
+  output$team_2_points_against <- renderInfoBox({
+    #browser()
+    points_against <- team_2_df()[1,3]
+    infoBox(
+      title = "Mean Points Against",
+      value = as.character(points_against) ,
+      icon = icon("fighter-jet"),
+      color = "red",
+      width = 12
+    )
+  })
+  
+  output$team_2_diff <- renderInfoBox({
+    #browser()
+    point_diff <- team_2_df()[1,4]
+    infoBox(
+      title = "Margin of Victory",
+      value = as.character(point_diff) ,
+      icon = icon("arrows-alt-v"),
+      color = "blue",
+      width = 12
+    )
+  })
+  
+})
+
+
